1e902343732a076ceaea4a01ee548465d15834fb
VladimirAn <vovanpro@inbox.ru>
2023-06-14 00:00:48 +0300

    Решил задачу 13 из домашнего задания к семинару 2

---
+++ Seminar_1/task13_dz_2seminar/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+else while (number > 999)
+{
+    number = number / 10;
+}
+    int ThridDigit = number % 10;
+Console.WriteLine($"Третяя цифра введенного числа {ThridDigit}");
