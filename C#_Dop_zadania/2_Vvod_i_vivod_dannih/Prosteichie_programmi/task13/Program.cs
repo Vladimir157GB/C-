9b677ae00ea537d1da8df396be2704627603630b
VladimirAn <vovanpro@inbox.ru>
2023-06-26 02:09:26 +0300

    Порешал доп задачки

---
+++ C#_Dop_zadania/2_Vvod_i_vivod_dannih/Prosteichie_programmi/task13/Program.cs
@@ -0,0 +1,9 @@
+﻿//Составить программу вывода на экран следующей информации:
+//2 кг
+//13 17
+
+string str = "2 кг";
+int numberA = 13;
+int numberB = 17;
+Console.WriteLine($"{str}");
+Console.WriteLine($"{numberA} {numberB}");
