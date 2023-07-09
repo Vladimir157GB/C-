9e98d6380e4d7fe9afa4c877e0abd9cf1b038d47
VladimirAn <vovanpro@inbox.ru>
2023-07-09 23:14:48 +0300

    Решил задачи ДЗ из 9 семинара

---
+++ Seminar_1/task64_dz_9seminar/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNumbersNto1(int numberN)
+{
+    if (numberN > 1)
+    {
+        System.Console.Write($"{numberN}, ");
+        PrintNumbersNto1(numberN - 1);
+    }
+    else
+    {
+        System.Console.Write($"{numberN}");
+    }
+}
+
+System.Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+PrintNumbersNto1(numberN);
---
+++ Seminar_1/task66_dz_9seminar/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+
+int SumNumbersFromN2M(int n, int m)
+{
+    int sum = 0;
+    if (n > m)
+    {
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+    }
+    else 
+    {
+        for (int i = n; i <= m; i++)
+        {
+            sum += i;
+        }  
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{SumNumbersFromN2M(numberN, numberM)}");
